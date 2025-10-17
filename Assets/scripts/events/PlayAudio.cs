509c4e36e479efdc179545b191e2290e38a9bb22
laimatt <67487124+laimatt@users.noreply.github.com>
2025-10-17 13:05:50 -0400

    added audio

---
+++ Assets/scripts/events/PlayAudio.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using events;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+// Attach this script to a GameObject with a UI Text (or other display) and wire it to
+// the CardContainer -> Events -> OnCardPlayed event in the Inspector.
+public class PlayAudio : MonoBehaviour {
+    [SerializeField]
+    private AudioSource soundEffectAudioSource;
+
+
+
+    public void Audio(CardPlayed evt) {
+        if (soundEffectAudioSource == null) {
+            Debug.LogWarning("PlayAudio: audio is not assigned.");
+            return;
+        }
+
+        // string path = Path.Combine(Application.streamingAssetsPath);
+
+        var cardName = evt?.card?.gameObject?.name ?? "bido";
+        // messageText.clip = cardName;
+
+        AudioClip clip = Resources.Load<AudioClip>(cardName);
+        Debug.LogWarning($"PlayAudio: {cardName} ");
+
+        soundEffectAudioSource.clip = clip;
+
+        soundEffectAudioSource.Play();
+
+        // var cardName = evt?.card?.gameObject?.name ?? "Card";
+        // messageText.text = $"Played: {cardName}";
+    }
+
+}
